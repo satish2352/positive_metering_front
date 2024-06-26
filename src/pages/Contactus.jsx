ec353b8e99d41c7ad1d639dc657a5528775eebc9
shubham <shubham.kothavade09@gmail.com>
2024-06-26 11:54:12 +0530

    changes

---
+++ src/pages/Contactus.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Ouroffices from '../PageComponents/Contactus/Ouroffices'
+import ConatactCard from '../PageComponents/Contactus/ConatactCard'
+import Shopnow from '../PageComponents/Home/Shopnow'
+import UploadCV from '../PageComponents/Contactus/UploadCV'
+
+const Contactus = () => {
+  return (
+    <>
+      <ConatactCard />
+      <Shopnow />
+      <Ouroffices />
+      <UploadCV/>
+
+    </>
+  )
+}
+
+export default Contactus
