dfb027ea78ac55493bdbc355c9da54ef786473ea
code-store-hj <155665884+code-store-hj@users.noreply.github.com>
2024-07-01 13:02:05 +0530

    changes in request callback

--- src/PageComponents/Home/Requestcallback.jsx
+++ src/PageComponents/Home/Requestcallback.jsx
@@ -2,9 +2,53 @@ import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../../assets/CSS/requestcall.css";
 const Requestcallback = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-    
-  
+  const [message, setMessage] = useState("");
+
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!name.trim()) {
+      errors.name = "Name is required";
+      isValid = false;
+    }
+
+    if (!email.trim()) {
+      errors.email = "email is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "invalid email address";
+      isValid = false;
+    }
+
+    if (!message.trim()) {
+      errors.message = "message is required";
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+  console.log("errors", errors);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      let newData = {
+        name,
+        email,
+
+        message,
+      };
+      alert("Data Submit Successfully...");
+      console.log("newData", newData);
+    }
+  };
 
   return (
     <div className=" requestcallback d-flex align-items-center justify-content-evenly ">
@@ -35,35 +79,52 @@ const Requestcallback = () => {
               </h4>
             </div>
             <div className="p-3 px-5">
-              <form action="" >
+              <form action="" onSubmit={handleSubmit}>
                 <div className=" d-grid fw-bold requestinputlb">
                   <label htmlFor=""> Name</label>
                   <input
                     type="text"
                     className="requestinput"
-                    placeholder="Enter name"
-
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                   />
+                  {errors.name && (
+                    <span className="error text-danger">{errors.name}</span>
+                  )}
                 </div>
                 <div className=" d-grid requestinputlb">
-                  <label htmlFor=""> Name</label>
+                  <label htmlFor=""> Email</label>
                   <input
                     type="text"
                     className="requestinput"
-                    placeholder="Enter name"
+                    placeholder="Enter Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
+                  {errors.email && (
+                    <span className="error text-danger">{errors.email}</span>
+                  )}
                 </div>
                 <div className=" d-grid requestinputlb">
-                  <label htmlFor=""> Name</label>
+                  <label htmlFor=""> Message</label>
                   <input
                     type="text"
                     className="requestinput"
-                    placeholder="Enter name"
+                    placeholder="comment"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                   />
+                  {errors.message && (
+                    <span className="error text-danger">{errors.message}</span>
+                  )}
                 </div>
                 <div className=" d-flex justify-content-end pt-4">
-                  <button className="px-4 py-2 text-center formrequestbtn " type="submit">
-                    Read More
+                  <button
+                    className="px-4 py-2 text-center formrequestbtn "
+                    type="submit"
+                  >
+                    Submit
                   </button>
                 </div>
               </form>
