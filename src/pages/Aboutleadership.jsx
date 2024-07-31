1add98e4745483fb17dd7cb10a790ac313fd3cec
shubham <shubham.kothavade09@gmail.com>
2024-07-31 15:27:00 +0530

    changes in owner

--- src/pages/Aboutleadership.jsx
+++ src/pages/Aboutleadership.jsx
@@ -18,29 +18,28 @@ const leadershipData = [
     name: "Mr. Sudhir Mutalik",
     title: "Founder And Managing Director",
     description:
-      "The Founder of our company Mr. Sudhir Mutalik is a passionate mechanical engineer graduated from the reputed Government College of Engineering, Karad near Pune, India. Diesel Engines has been his interest for study since his university days. He was selected by the world's well-known Diesel Engine manufacturer Kirloskar Oil Engines Ltd through campus recruitment.",
+      "Mr. Sudhir Mutalik, a first-generation entrepreneur, has consistently prioritized people while building his business from the ground up. He measures success by the number of jobs his company generates annually, reflecting his commitment to society and nature. Mr. Mutalik served as the Chairman of CII's Maharashtra State Council in 2021-22. His commendable efforts to aid people and industries in the state during the first and second waves of COVID-19 were officially recognized by the Honorable Governor of Maharashtra. Mr. Mutalik was honored as an 'Impact Creator' in a ceremony at Raj Bhavan. His personal goal is to foster entrepreneurship, inspiring and guiding young individuals to start their own enterprises."
   },
   {
     imgSrc: lder2,
     name: "Mrs. Jyotsana Mutalik",
     title: "CEO Finance Director",
     description:
-      "The Founder of our company Mr. Sudhir Mutalik is a passionate mechanical engineer graduated from the reputed Government College of Engineering, Karad near Pune, India. Diesel Engines has been his interest for study since his university days. He was selected by the world's well-known Diesel Engine manufacturer Kirloskar Oil Engines Ltd through campus recruitment.",
+      "Jyotsna Mutalik is a first-generation entrepreneur who has been managing various areas of business, with a strong focus on finance and costing. She is known for her critical thinking and problem-solving skills, making her a beloved figure within the organization. Over the years, she has trained many young professionals. Jyotsna played a pivotal role in achieving ISO certification for the organization and has consistently maintained this status. Her passion for digitalization led to the development of an in-house ERP system, which has been in operation since 2020 and continues to evolve to meet the organization's needs.",
   },
   {
     imgSrc: lder3,
     name: "Mr. Malhar Mutalik",
     title: "Cheif Technical Manager",
     description:
-      "The Founder of our company Mr. Sudhir Mutalik is a passionate mechanical engineer graduated from the reputed Government College of Engineering, Karad near Pune, India. Diesel Engines has been his interest for study since his university days. He was selected by the world's well-known Diesel Engine manufacturer Kirloskar Oil Engines Ltd through campus recruitment.",
+      "Mr. Malhar Sudhir Mutalik, 27, is a second-generation entrepreneur and the chief operating officer (COO) of the pumps division. He graduated as a mechanical engineer in 2019 and now leads the company's R&D wing. In 2018, he designed mechanical diaphragm pumps, leading to a new business vertical that dispatched over 2000 pumps in fiscal 2024. The following year, he developed GRM pumps with electrical actuators, making PMP the only Indian manufacturer with this technology, with the first consignment sent to Uzbekistan. In 2020, he designed and manufactured API-compliant pressure relief valves now used by major refineries in India and abroad. By 2022, he led the development of metallic diaphragm pumps, positioning PMP as one of three Indian companies with this product.",
   },
   {
     imgSrc: lder2,
-    name: "Mr. Ravindra Jadhav",
-    title: "Head Operations Pump's Businessr",
+    name: "Ms. Madhuli Mutalik",
+    title: "Chief Material Manager",
     description:
-      "The Founder of our company Mr. Sudhir Mutalik is a passionate mechanical engineer graduated from the reputed Government College of Engineering, Karad near Pune, India. Diesel Engines has been his interest for study since his university days. He was selected by the world's well-known Diesel Engine manufacturer Kirloskar Oil Engines Ltd through campus recruitment",
-  },
+      "Ms. Madhuli Mutalik is a second-generation entrepreneur and an integral part of Positive Metering Pumps (I) Pvt Ltd. She has been actively involved with the company for several years, starting her journey by mastering inventory management. Madhuli transformed our material flow by introducing practical approaches and implementing highly effective processes with guidance. She actively participates in material cost negotiations, aiming for win-win solutions that meet both parties' needs and goals. Together with her team, Madhuli ensures optimal inventory levels at all stages, from raw materials to final pump assembly, meeting production demands while minimizing excess stock. This includes generating appropriate reports, monitoring inventory levels and costs, and implementing .",   },
 ];
 
 const Aboutleadership = () => {
@@ -88,7 +87,7 @@ const Aboutleadership = () => {
                   data-aos-duration="2000"
                 >
                   <div>
-                    <h2
+                    <h3
                       className="text-uppercase abtname fw-bold"
                       style={{
                         letterSpacing: "1px",
@@ -97,8 +96,8 @@ const Aboutleadership = () => {
                       }}
                     >
                       {leader.name}
-                    </h2>
-                    <h4 style={{ paddingLeft: "20px" }}>{leader.title}</h4>
+                    </h3>
+                    <h5 style={{ paddingLeft: "20px" }}>{leader.title}</h5>
                     <p
                       className="fw-medium abouttxt lh-base"
                       style={{
@@ -110,7 +109,7 @@ const Aboutleadership = () => {
                     >
                       {leader.description}
                     </p>
-                    <div style={{ paddingLeft: "20px" }}className="social-icons">
+                    <div style={{ paddingLeft: "20px" }} className="social-icons">
                       <FaFacebook className="icon" />
                       <FaInstagram className="icon" />
                       <FaLinkedin className="icon" />
@@ -145,7 +144,7 @@ const Aboutleadership = () => {
                   data-aos-duration="2000"
                 >
                   <div>
-                    <h2
+                    <h3
                       className="text-uppercase abtname fw-bold"
                       style={{
                         letterSpacing: "1px",
@@ -154,8 +153,8 @@ const Aboutleadership = () => {
                       }}
                     >
                       {leader.name}
-                    </h2>
-                    <h4 className="" style={{ paddingLeft: "20px" }}>{leader.title}</h4>
+                    </h3>
+                    <h5 className="" style={{ paddingLeft: "20px" }}>{leader.title}</h5>
                     <p
                       className="fw-medium abouttxt lh-base"
                       style={{
